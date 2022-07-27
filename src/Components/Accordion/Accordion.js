dba940c6f2a5f3cf06b0cb814903298243d69644
StanTheMan7 <sts.graur@gmail.com>
2022-07-27 21:54:26 +0200

    accoredion-menu with multiple sections and dynamic data

--- src/Components/Accordion/Accordion.js
+++ src/Components/Accordion/Accordion.js
@@ -1,29 +1,34 @@
 import React, {useRef, useState, useEffect} from "react";
 import './Accordion.css'
-import Chevron from '../Assets/Icons/down-chevron.png'
-import ChevronUp from '../Assets/Icons/up-chevron.png'
+import Chevron from '../../Assets/Icons/down-chevron.png'
+import ChevronUp from '../../Assets/Icons/up-chevron.png'
 
-export default function Accordion() {
 
-    const [isActive, setIsActive] = useState(false);
-    const [heightEl, setHeightEl] = useState();
+export default function Accordion({title, content}) {
+          
 
-    const toggleIsActive = () => {
-        setIsActive(!isActive);
-    }
     
-    const refHeight = useRef();
+          const [isActive, setIsActive] = useState(false);
 
-    useEffect(() => {
-        setHeightEl(`${refHeight.current.scrollHeight}px`);
-    }, [])
+          const [heightEl, setHeightEl] = useState();
+          
+          const toggleIsActive = () => {
+              setIsActive(!isActive);
+          }
+          const refHeight = useRef();
+
+
     
+
+          useEffect(() => {
+              setHeightEl(`${refHeight.current.scrollHeight}px`);
+          }, [])
 
     return (
       <>
         <div className="accordion">
           <div className="accordion-visible" onClick={toggleIsActive}>
-            <h2>My accordion title</h2>
+            <h2>{title}</h2>
 
             <img
               className="chevron-down"
@@ -41,14 +46,11 @@ export default function Accordion() {
           </div>
 
           <div ref={refHeight} className={isActive ? 'toggle-isActive animated' : "toggle-isActive"} style={{height : isActive ? `${heightEl}` : '0px'}}>
+            
             <p aria-hidden={isActive ? "true" : "false"}>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam
-              alias illum, quam soluta dolorum harum iste neque nulla nemo
-              earum, sit fugiat eius, iure laborum expedita. Nemo, tenetur porro
-              ab praesentium possimus soluta nulla dolorum placeat atque saepe
-              consequatur fugiat animi natus non libero sed quibusdam, fuga
-              quaerat assumenda exercitationem?
+              {content}
             </p>
+          
           </div>
         </div>
       </>
