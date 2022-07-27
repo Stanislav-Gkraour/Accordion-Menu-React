c76e36d74b02de621836ccc5bb7771f116c6b58d
StanTheMan7 <sts.graur@gmail.com>
2022-07-27 22:24:59 +0200

    unic key added trough the uuidv4(), every child has a dif ID

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Accordion from './Components/Accordion/Accordion';
 import './App.css';
+import {v4 as uuidv4} from 'uuid'
 
 function App() {
   const accordionData = [
@@ -10,6 +11,7 @@ function App() {
                 laborum cupiditate possimus labore, hic temporibus velit dicta earum
                 suscipit commodi eum enim atque at? Et perspiciatis dolore iure
                 voluptatem.`,
+      id: uuidv4()
     },
     {
       title: "Section 2",
@@ -19,20 +21,23 @@ function App() {
                 pariatur? Impedit autem esse nostrum quasi, fugiat a aut error cumque
                 quidem maiores doloremque est numquam praesentium eos voluptatem amet!
                 Repudiandae, mollitia id reprehenderit a ab odit!`,
+      id: uuidv4()
     },
     {
-      title: "Section 3",
+      title: `Section 3`,
       content: `Sapiente expedita hic obcaecati, laboriosam similique omnis architecto ducimus magnam accusantium corrupti
                 quam sint dolore pariatur perspiciatis, necessitatibus rem vel dignissimos
                 dolor ut sequi minus iste? Quas?`,
+      id: uuidv4()
     },
   ];
-  const { title, content } = accordionData;
+  console.log(accordionData[0], accordionData[1],accordionData[2]);
+  const { title, content, id } = accordionData;
   return (
     <>
       <h1>React Accordion Demo</h1>
-      {accordionData.map(({ title, content }) => (
-        <Accordion title={title} content={content} />
+      {accordionData.map(({ title, content,id  }) => (
+        <Accordion title={title} content={content} key={id} />
       ))}
     </>
   );
